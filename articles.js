a8692cdc4c730f7b82605235c21d2ed3e24b3968
Pulkit-2002 <141421176+Pulkit-2002@users.noreply.github.com>
2024-01-12 01:32:44 +0530

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,14 @@
+// routes/articles.js
+const express = require('express');
+const router = express.Router();
+const authenticateToken = require('../middlewares/authMiddleware');
+const Article = require('../models/article');
+
+router.get('/protected', authenticateToken, (req, res) => {
+    // Only accessible with a valid token
+    res.json({ message: 'This is a protected route' });
+});
+
+// Add more protected routes as needed
+
+module.exports = router;
